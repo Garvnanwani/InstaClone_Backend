7fdcb492a062b0c987fdef699d73a8688c3942cd
Garv nanwani <garvnanwani16@gmail.com>
2020-10-02 18:53:30 +0530

    created auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startswith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return next({
+            message: "You need to be logged in to visit this route",
+            statusCode: 403,
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user) {
+            return next({ message: `No user found for ID ${decoded.id}` })
+        }
+
+    } catch (err) {
+        next({
+            message: "You need to be logged in to visit this route",
+            statusCode = 403
+        })
+    }
+
+}
+
+
+module.exports = protect;
