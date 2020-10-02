a912ece2591e3d5e26138f9aebd55d2bcfc48ac8
Garv nanwani <garvnanwani16@gmail.com>
2020-10-02 18:22:33 +0530

    created User model

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
-const connectDB = require('./config/db');
+const connectDB = require('./utils/db');
 const errorHandler = require('./middlewares/errorHandler');
 require('dotenv').config();
 
